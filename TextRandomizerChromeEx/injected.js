5eaecc45aea308df0eb0aeb1ef629b66a6dcbf21
Dusan Togner <togner@gmail.com>
2013-12-18 13:13:42 -0500

    Cleaned up content script, added traverseText method.

--- TextRandomizerChromeEx/injected.js
+++ TextRandomizerChromeEx/injected.js
@@ -1,45 +1,3 @@
-function onRequest(request, sender, sendResponse) {
-	console.log(request);
-	var ignore = { "STYLE":0, "SCRIPT":0, "NOSCRIPT":0, "IFRAME":0, "OBJECT":0 };
-
-	// todo: clean the code - letterTraversal(transform fn), wordTraversal(transform fn)
-	if (request.action == "getHistogram") {
-		sendResponse(buildWordHistogram());
-	} else if (request.action == "swapWords") {
-		swapWords(request);
-	} else if (request.action == "randomize") {
-		$("*").each(function() { 
-			var jthis = $(this);
-			if (jthis.children().length == 0) {
-				if (!(jthis.prop("tagName") in ignore)) {
-					var text = jthis.text();
-					if (text != "" &&  text.trim().length > 0) {
-						// replace text of node based on the vowel/consonant swap maps
-						var result = "";
-						var textLen = text.length;
-						for (var i = 0; i < textLen; i++) {
-							var c = text[i].toLowerCase();
-							var isUpper = (c != text[i]);
-							var vowelIndex = request.vowelSwapMap.from.indexOf(c);
-							var consonantIndex = request.consonantSwapMap.from.indexOf(c);
-							if (vowelIndex != -1) {
-								c = request.vowelSwapMap.to[vowelIndex];
-							} else if (consonantIndex != -1) {
-								c = request.consonantSwapMap.to[consonantIndex];
-							}
-							if (isUpper) {
-								c = c.toUpperCase();
-							}
-							result += c;
-						}
-						jthis.text(result);
-					}
-				}
-			} 
-		});
-	}
-}
-
 String.prototype.capitalize = function()
 {
     return this.charAt(0).toUpperCase() + this.slice(1);
@@ -84,8 +42,81 @@ function swap(text, wordSwaps) {
 }
 
 function swapWords(request) {
-	var ignore = { "STYLE":0, "SCRIPT":0, "NOSCRIPT":0, "IFRAME":0, "OBJECT":0 };
+	traverseText(function() {
+		var text = this.text();
+		if (text == "" ||  text.trim().length == 0) {
+			return;
+		}
+		text = swap(text, request.wordSwaps);
+		this.text(text);
+	});
+}
 
+function swapLetters(request) {
+	traverseText(function() {
+		var text = this.text();
+		if (text == "" ||  text.trim().length == 0) {
+			return;
+		}
+		
+		// replace text of node based on the vowel/consonant swap maps
+		var result = "";
+		var textLen = text.length;
+		for (var i = 0; i < textLen; i++) {
+			var c = text[i].toLowerCase();
+			var isUpper = (c != text[i]);
+			var vowelIndex = request.vowelSwapMap.from.indexOf(c);
+			var consonantIndex = request.consonantSwapMap.from.indexOf(c);
+			if (vowelIndex != -1) {
+				c = request.vowelSwapMap.to[vowelIndex];
+			} else if (consonantIndex != -1) {
+				c = request.consonantSwapMap.to[consonantIndex];
+			}
+			if (isUpper) {
+				c = c.toUpperCase();
+			}
+			result += c;
+		}
+		this.text(result);
+	});
+}
+
+function buildWordHistogram() {
+	var wordCount = 0;
+	var histogram = {};
+	traverseText(function() {
+		var text = this.text();
+		var textWords = text.match(/\w+/g);
+		
+		if (!textWords) {
+			return;
+		}
+		
+		var textWordsLen = textWords.length;
+		
+		// valid word
+		if (textWordsLen > 0) {
+			wordCount += textWordsLen;
+			
+			// normalize
+			for (var i = 0; i < textWordsLen; i++) {
+				var word = textWords[i].toLowerCase();
+				if (histogram.hasOwnProperty(word)) {
+					histogram[word]++;
+				} else {
+					histogram[word] = 1;
+				}
+			}
+		}
+	});
+	return {
+		wordCount: wordCount,
+		histogram: histogram
+	};
+}
+
+function traverseText(func) {
+	var ignore = { "STYLE":0, "SCRIPT":0, "NOSCRIPT":0, "IFRAME":0, "OBJECT":0 };
 	$("*").each(function() { 
 		var jthis = $(this);
 		if (jthis.children().length == 0) {
@@ -93,64 +124,20 @@ function swapWords(request) {
 			if ((jthis.prop("tagName") in ignore)) {
 				return;
 			}
-			var text = jthis.text();
-			
-			if (text == "" ||  text.trim().length == 0) {
-				return;
-			}
-			
-			text = swap(text, request.wordSwaps);
-			
-			jthis.text(text);
+			func.call(jthis);
 		}
 	});
 }
 
-function buildWordHistogram() {
-	var ignore = { "STYLE":0, "SCRIPT":0, "NOSCRIPT":0, "IFRAME":0, "OBJECT":0 };
-	
-	var wordCount = 0;
-	var histogram = {};
-	
-	$("*").each(function() { 
-		var jthis = $(this);
-        if (jthis.children().length == 0) {
-		
-			// ignore markup
-			if ((jthis.prop("tagName") in ignore)) {
-				return;
-			}
-			
-			var text = jthis.text();
-			var textWords = text.match(/\w+/g);
-			
-			if (!textWords) {
-				return;
-			}
-			
-			var textWordsLen = textWords.length;
-			
-			// valid word
-			if (textWordsLen > 0) {
-				wordCount += textWordsLen;
-				
-				// normalize
-				for (var i = 0; i < textWordsLen; i++) {
-					var word = textWords[i].toLowerCase();
-					if (histogram.hasOwnProperty(word)) {
-						histogram[word]++;
-					} else {
-						histogram[word] = 1;
-					}
-				}
-			}
-		}
-	});
-	
-	return {
-		wordCount: wordCount,
-		histogram: histogram
-	};
+function onRequest(request, sender, sendResponse) {
+	console.log(request);
+	if (request.action == "getHistogram") {
+		sendResponse(buildWordHistogram());
+	} else if (request.action == "swapWords") {
+		swapWords(request);
+	} else if (request.action == "randomize") {
+		swapLetters(request);
+	}
 }
 
 chrome.runtime.onMessage.addListener(onRequest);
